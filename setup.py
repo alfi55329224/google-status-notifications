3c48f2e2ddc1f6729919b2ce4058dcf6c027e615
Damir Suleymanov <damir@cloudlock.com>
2014-11-13 21:56:20 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='google-status-updates',
+      version='0.0.1',
+      description='Google Status Updates',
+      author='Damir Suleymanov',
+      author_email='damir@cloudlock.com',
+      url='http://github.com/bitle',
+      )
