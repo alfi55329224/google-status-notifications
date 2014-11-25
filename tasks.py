8fd0c4534d9a9797bf9660dede0e4ae7187424e9
Damir Suleymanov <damir@cloudlock.com>
2014-11-25 17:25:19 -0500

    Save state and notify of changes only if state has changed

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ import os
 from celery import Celery
 import feedparser
 import requests
+import redis
 
 app = Celery('worker')
 app.conf.update(
@@ -21,7 +22,7 @@ app.conf.update(
         }
     }
 )
-
+redis_client = redis.from_url(os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'))
 log = logging.getLogger(__name__)
 
 
@@ -33,11 +34,15 @@ def smoke_test():
 
 @app.task(ignore_results=True)
 def check_feed():
+    prev_items_count = redis_client.get("num_items", 0)
     d = feedparser.parse('http://www.google.com/appsstatus/rss/en')
     if d.entries:
         for item in d.entries:
             log.info("%s\t%s: %s", item.published, item.title, item.description)
-        send_simple_message()
+
+        if prev_items_count < len(d.entries):
+            redis_client.set("num_items", len(d.entries))
+            send_simple_message()
     else:
         log.info("No entries")
 
